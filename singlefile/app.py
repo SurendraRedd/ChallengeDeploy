8a5c7c0aeced6153e310edf85e42d0f07f48d698
Surendra Reddy <37290660+SurendraRedd@users.noreply.github.com>
2020-11-07 21:16:51 +0530

    Update app.py

--- singlefile/app.py
+++ singlefile/app.py
@@ -130,9 +130,9 @@ def main():
         st.write("All adults have a right to vote, but this right is not being exercised due to thelimitation of voting in the registered constituency by being physically there.")
 
         st.subheader("Proposed Solution:")
-        #st.write("Refer the web application solution for the last vote challenges.")
-        img = Image.open("https://github.com/SurendraRedd/ChallengeDeploy/raw/main/singlefile/Solution.png")
-        st.image(img, width=750, caption="Solution")
+        st.write("Refer the web application solution for the last vote challenges.")
+        #img = Image.open("https://github.com/SurendraRedd/ChallengeDeploy/raw/main/singlefile/Solution.png")
+        #st.image(img, width=750, caption="Solution")
 
     elif choice == "Login":
       st.subheader("**Please Login to avail the vote!**")
